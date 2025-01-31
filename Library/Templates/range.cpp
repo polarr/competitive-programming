2a84828e2819cd03c315a6c31a69b657664e9b94
polarr <charles.ran9@gmail.com>
2025-01-31 01:00:20 -0800

    lib: add segment tree, recursive segment tree to range, add topological sort to tree

--- Library/Templates/range.cpp
+++ Library/Templates/range.cpp
@@ -81,3 +81,101 @@ template <class T> class FT {
 		return total;
 	}
 };
+
+/** 
+ * DATASTRUCTURE: Segment Tree
+ * PURPOSE: Range Updates and Queries
+ * TIME: O(log n) to update and query
+*/
+template <class T> class SegmentTree {
+  private:
+	const T UNIT = std::numeric_limits<T>().max();
+
+    T combine(T a, T b){
+        return min(a, b);
+    }
+
+	vector<T> segtree;
+	int len;
+
+  public:
+	SegmentTree(int len) : len(len), segtree(len * 2, UNIT) {}
+
+	void set(int ind, T val) {
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind / 2] = combine(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+
+	T query(int start, int end) {
+		T ans = UNIT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if (start % 2 == 1) { ans = combine(ans, segtree[start++]); }
+			if (end % 2 == 1) { ans = combine(ans, segtree[--end]); }
+		}
+		return ans;
+	}
+};
+
+/** 
+ * DATASTRUCTURE: Recursive Segment Tree
+ * PURPOSE: Segment Tree with more extensibility
+ * TIME: O(log n) to update and query, but worse constant factor than the iterative version
+*/
+template <class T> class RecSegmentTree {
+  private:
+	const T UNIT = 0;
+
+	int len;
+	vector<T> segtree;
+
+	T combine(const T &a, const T &b) { return a + b; }
+
+	void build(const vector<T> &arr, int at, int at_left, int at_right) {
+		if (at_left == at_right) {
+			segtree[at] = arr[at_left];
+			return;
+		}
+		int mid = (at_left + at_right) / 2;
+		build(arr, 2 * at, at_left, mid);
+		build(arr, 2 * at + 1, mid + 1, at_right);
+		segtree[at] = combine(segtree[2 * at], segtree[2 * at + 1]);
+	}
+
+	void set(int ind, T val, int at, int at_left, int at_right) {
+		if (at_left == at_right) {
+			segtree[at] = val;
+			return;
+		}
+		int mid = (at_left + at_right) / 2;
+		if (ind <= mid) {
+			set(ind, val, 2 * at, at_left, mid);
+		} else {
+			set(ind, val, 2 * at + 1, mid + 1, at_right);
+		}
+		segtree[at] = combine(segtree[2 * at], segtree[2 * at + 1]);
+	}
+
+	T range_sum(int start, int end, int at, int at_left, int at_right) {
+		if (at_right < start || end < at_left) { return UNIT; }
+		if (start <= at_left && at_right <= end) { return segtree[at]; }
+		int mid = (at_left + at_right) / 2;
+		T left_res = range_sum(start, end, 2 * at, at_left, mid);
+		T right_res = range_sum(start, end, 2 * at + 1, mid + 1, at_right);
+		return combine(left_res, right_res);
+	}
+
+  public:
+	RecSegmentTree(int len) : len(len) { segtree = vector<T>(len * 4, UNIT); };
+
+	RecSegmentTree(const vector<T> &arr) : len(arr.size()) {
+		segtree = vector<T>(len * 4, UNIT);
+		build(arr, 1, 0, len - 1);
+	}
+
+	void set(int ind, T val) { set(ind, val, 1, 0, len - 1); }
+
+	T range_sum(int start, int end) { return range_sum(start, end, 1, 0, len - 1); }
+};
--- Library/Templates/tree.cpp
+++ Library/Templates/tree.cpp
@@ -19,3 +19,46 @@ using pii = pair<int, int>;
 const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 const ll INF = LLONG_MAX;
+
+/** 
+ * ALGORITHM: Topological Sort
+ * PURPOSE: Sorts a DAG IN-PLACE such that each directed edge points forward
+ * CONSTRAINT: Graph must be a DAG
+ * TIME: O(V)
+*/
+vector<int> toposort(int n, vector<vector<int>> &adj){
+    vector<int> degree(n, 0);
+    queue<int> q; // use pq for lexicographical sorting
+    for (int i = 0; i < n; i++){
+        for (int j : adj[i]){
+            degree[j]++;
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        if (degree[i] == 0){
+            q.push(i);
+        }
+    }
+
+    vector<int> ans;
+
+    while(!q.empty()){
+        int i = q.top();
+        ans.pb(i);
+        q.pop();
+
+        for (int j : adj[i]){
+            if (degree[j] == 0){
+                continue;
+            }
+
+            degree[j]--;
+            if (degree[j] == 0){
+                q.push(j);
+            }
+        }
+    }
+
+    return ans;
+}
