7315031105ea5033a6b808925234e22b943d8689
polarr <charles.ran9@gmail.com>
2025-02-14 23:42:54 -0800

    lib: add sparse table to range, euler tour, lca to tree

--- Library/Templates/tree.cpp
+++ Library/Templates/tree.cpp
@@ -4,6 +4,7 @@
 **/
 
 #include <bits/stdc++.h>
+#include "./range.cpp"
 using namespace std;
 
 using ull = unsigned long long;
@@ -62,3 +63,82 @@ vector<int> toposort(int n, vector<vector<int>> &adj){
 
     return ans;
 }
+
+/** 
+ * ALGORITHM: Euler Tour
+ * PURPOSE: Flattens a tree so that each range contains any subtree range
+ * CONSTRAINT: Graph must be a tree
+ * TIME: O(V)
+*/
+vector<pii> eulerTour(int n, vector<vector<int>> &adj){
+    vector<pii> ans(n);
+    int i = -1;
+    function<void(int, int)> tour;
+    tour = [&](int node, int parent){
+        ans[node].first = ++i;
+        for (int x : adj[node]){
+            if (x != parent){
+                tour(x, node);
+            }
+        }
+        ans[node].second = i;
+    };
+
+    // root at 0
+    tour(0, 0);
+
+    return ans;
+}
+
+/**
+ * ALGORITHM: Lowest Common Ancestor
+ * PURPOSE: Lowest common ancestor node of two nodes, also supports depth and distance of nodes
+ * TIME: O(n) preprocessing, O(log N) lca query
+ * REQUIRE: Segment Tree (or another RMQ structure)
+ */
+class LCA {
+    private: 
+        /** IN SEGTREE REMEMBER TO SET UNIT = pii{INT_MAX, INT_MAX} */
+        SegmentTree<pii> st;
+        vi first;
+        vi tour;
+        vi d;
+
+    public:
+        LCA(int n, vector<vector<int>> &adj) : st(2 * n - 1), first(n), tour(2 * n - 1), d(n) {
+            int i = -1;
+            function<void(int, int, int)> dfs;
+            dfs = [&](int node, int parent, int depth){
+                tour[++i] = node;
+                first[node] = i;
+                d[node] = depth;
+                for (int x : adj[node]){
+                    if (x != parent){
+                        dfs(x, node, depth + 1);
+                        tour[++i] = node;
+                    }
+                }
+            };
+
+            // root at 0
+            dfs(0, 0, 0);
+
+            rep(i, 0, 2 * n - 1){
+                st.set(i, {d[tour[i]], tour[i]});
+            }
+        }
+
+        int depth(int node){
+            return d[node];
+        }
+
+        int lca(int a, int b){
+            pii range = minmax(first[a], first[b]);
+            /** IN SPARSE TABLE REMEMBER TO USE INCLUSIVE BOUND range.second */
+            return st.query(range.first, range.second + 1).second;
+        }
+
+        int dist(int a, int b){
+            return d[a] + d[b] - 2 * d[lca(a, b)];
+        }
+};
