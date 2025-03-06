c3f6ba80e018bef7ae8fee5dbf9266d5cd659c1b
polarr <charles.ran9@gmail.com>
2025-03-05 23:31:06 -0800

    lib: add sparse lazy segment tree to range

--- Library/Templates/range.cpp
+++ Library/Templates/range.cpp
@@ -182,6 +182,95 @@ template <class T> class RecSegmentTree {
 	T range_sum(int start, int end) { return range_sum(start, end, 1, 0, len - 1); }
 };
 
+/**
+ * DATASTRUCTURE: Sparse Segment Tree
+ * PURPOSE: Lazy Segment Tree on large intervals
+ * TIME: O(log n)
+ * MEMORY: O(q log n)
+ * SOURCE: KACTL
+ */
+static char buf[450 << 20];
+void* operator new(size_t s) {
+	static size_t i = sizeof buf;
+	assert(s < i);
+	return (void*)&buf[i -= s];
+}
+void operator delete(void*) {}
+template<class T> struct ptr {
+	unsigned ind;
+	ptr(T* p = 0) : ind(p ? unsigned((char*)p - buf) : 0) {
+		assert(ind < sizeof buf);
+	}
+	T& operator*() const { return *(T*)(buf + ind); }
+	T* operator->() const { return &**this; }
+	T& operator[](int a) const { return (&**this)[a]; }
+	explicit operator bool() const { return ind; }
+};/**
+ * DATASTRUCTURE: Sparse Segment Tree
+ * PURPOSE: Lazy Segment Tree on large intervals
+ * TIME: O(log n)
+ * MEMORY: O(q log n)
+ * SOURCE: KACTL
+ */
+struct Node {
+	Node *l = 0, *r = 0;
+    ll inf = 0;
+	ll lo, hi, mset = inf, madd = 0, val = -inf;
+	Node(ll lo,ll hi):lo(lo),hi(hi){} // Large interval of -inf
+	Node(vi& v, ll lo, ll hi) : lo(lo), hi(hi) {
+		if (lo + 1 < hi) {
+			ll mid = lo + (hi - lo)/2;
+			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
+			val = max(l->val, r->val);
+		}
+		else val = v[lo];
+	}
+    // NOTE: [L, R)
+	ll query(ll L, ll R) {
+		if (R <= lo || hi <= L) return -inf;
+		if (L <= lo && hi <= R) return val;
+		push();
+		return max(l->query(L, R), r->query(L, R));
+	}
+	void set(ll L, ll R, ll x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) mset = val = x, madd = 0;
+		else {
+			push(), l->set(L, R, x), r->set(L, R, x);
+			val = max(l->val, r->val);
+		}
+	}
+	void add(ll L, ll R, ll x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) {
+			if (mset != inf) mset += x;
+			else madd += x;
+			val += x;
+		}
+		else {
+			push(), l->add(L, R, x), r->add(L, R, x);
+			val = max(l->val, r->val);
+		}
+	}
+	void push() {
+		if (!l) {
+			ll mid = lo + (hi - lo)/2;
+			l = new Node(lo, mid); r = new Node(mid, hi);
+		}
+		if (mset != inf)
+			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
+		else if (madd)
+			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+	}
+};
+
+void releaseNode(Node* n)
+{
+    if (!n) return;
+    releaseNode(n->l);
+    releaseNode(n->r);
+    delete n;
+}
 
 /**
  * DATASTRUCTURE: Sparse Table
