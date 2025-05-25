044edce9ed3a18bd2bd56f38a87a78184fe5e14e
polarr <charles.ran9@gmail.com>
2025-05-24 22:12:50 -0700

    practice: codeforces 2110 f

--- Codeforces/2110/F.cpp
+++ Codeforces/2110/F.cpp
@@ -21,8 +21,67 @@ using pii = pair<int, int>;
 const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 
+// solved in 30 mins!!
+
 void solve(){
-    
+    int n; cin >> n;
+
+    vi a(n);
+    rep(i, 0, n){
+        cin >> a[i];
+    }
+
+    int mx = a[0];
+    int smx = 0;
+    int ans = 0;
+
+    map<int, bool> taken;
+    taken[a[0]] = true;
+
+    cout << ans << endl;
+
+    set<int> possible;
+
+    rep(i, 1, n){
+        int x = a[i];
+        if (taken[x]){
+            cout << ans << " ";
+            continue;
+        }
+
+        taken[x] = true;
+        if (x > mx){
+            possible.insert(mx);
+            if (x < 2 * mx){
+                ans = max(ans, x);
+            } else {
+                while (!possible.empty()){
+                    auto j = possible.begin();
+                    if (*j < mx/2){
+                        possible.erase(j);
+                    } else {
+                        break;
+                    }
+                }
+
+                for (int y : possible){
+                    ans = max(ans, y + (x % y));
+                }
+            }
+            
+            smx = mx;
+            mx = x;
+        }
+        else if (x >= smx/2){
+            smx = max(smx, x);
+            ans = max(ans, x + (mx % x));
+            possible.insert(x);
+        }
+
+        cout << ans << " ";
+    }
+
+    cout << endl;
 }
 
 int main(){
