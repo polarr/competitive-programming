c3953c2fba982986310d37a5c18ebb60f5d0bdc1
polarr <charles.ran9@gmail.com>
2025-09-17 18:56:23 -0700

    practice: codeforces 2094 h

--- Codeforces/2094/H.cpp
+++ Codeforces/2094/H.cpp
@@ -1,3 +1,10 @@
+/**
+ * Solution by Charles Ran (polarity.sh)
+ * Date: 2025-09-17
+ * Contest: Codeforces 2094
+ * Problem: H
+**/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,77 +21,68 @@ using pii = pair<int, int>;
 const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
 
+vector<vi> divs(MAX_N);
+
 void solve(){
-    int n, q; cin >> n >> q;
+    vi has;
+    int n, q; cin >> n >> q;  
 
-    vi a(n);
-    rep(i, 0, n){
-        cin >> a[i];
+    vi arr(n);
+    rep(i, 0, n) {
+        cin >> arr[i];
+        divs[arr[i]].pb(i);
+        has.pb(arr[i]);
     }
 
-    // vi b;
-    // vi make(n + 1);
-    // int cnt = 0;
-    // rep(i, 0, n){
-    //     if (i > 0 && a[i] != a[i - 1]){
-    //         cnt++;
-    //         b.pb(a[i]);
-    //     }
-
-    //     make[i] = cnt;
-    // }
-
-    // make[n] = cnt + 1;
-    
-    vector<vector<int>> has(MAX_N);
-
-    vector<vector<pii>> queries(n + 1);
-    vl ans(q);
-    rep(i, 0, q){
+    rep(ii, 0, q){
         int k, l, r; cin >> k >> l >> r;
-        --l;
-        queries[l].pb({k, i});
-        queries[r].pb({-1, i});
-    }
+        --l; --r;
+        ll ans = 0;
+        int curr = l;
+        while (curr <= r){
+            int mn = r + 1;
+            int j = 0;
 
-    vector<bool> gone(q, false);
-    vl change(q, -1);
-    vl curr(q, -1);
-
-    rep(i, 0, n + 1){
-        for (auto &[v, idx] : queries[i]){
-            if (v == -1){
-                gone[idx] = true;
-                ans[idx] += curr[idx] * (ll)(i - change[idx]);
-            } else {
-                has[v].pb(idx);
-                curr[idx] = v;
-                change[idx] = i;
+            if (k == 1){
+                ans += (r + 1 - curr);
+                break;
             }
-        }
 
-        if (i == n) continue;
-
-        for (int j = a[i]; j < MAX_N; j += a[i]){
-            for (int idx : has[j]){
-                if (gone[idx]) continue;
-                int jj = j;
-                while (jj % a[i] == 0){
-                    jj /= a[i];
+            rep(i, 1, sqrt(k) + 1){
+                if (k % i != 0) continue;
+                if (i != 1){
+                    int x = i;
+                    auto nx = lower_bound(all(divs[x]), curr);
+                    if (nx != divs[x].end()){
+                        if (*nx < mn) {
+                            mn = *nx;
+                            j = x;
+                        }
+                    }
                 }
-
-                has[jj].pb(idx);
-                ans[idx] += curr[idx] * (ll)(i - change[idx]);
-                curr[idx] = jj;
-                change[idx] = i;
+                int x = k/i;
+                auto nx = lower_bound(all(divs[x]), curr);
+                if (nx != divs[x].end()){
+                    if (*nx >= mn) {
+                        continue;
+                    }
+                    mn = *nx;
+                    j = x;
+                }
             }
 
-            has[j] = {};
+            ans += (ll) k * (mn - curr);
+            while (j != 0 && k % j == 0){
+                k /= j;
+            }
+            curr = mn;
         }
+
+        cout << ans << endl;
     }
 
-    for (ll a : ans){
-        cout << a << endl;
+    for (int x : has){
+        divs[x] = {};
     }
 }
 
