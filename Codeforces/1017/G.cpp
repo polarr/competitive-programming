6344e6d36b6fa159b5c5cff25b617b72835a6986
polarr <charles.ran9@gmail.com>
2025-10-13 20:39:34 -0700

    practice: codeforces 1017 g, hackerrank subtrees-and-paths

---
+++ Codeforces/1017/G.cpp
@@ -0,0 +1,255 @@
+/**
+ * Solution by Charles Ran (polarity.sh)
+ * Date: 2025-10-13
+ * Contest: Codeforces 1017
+ * Problem: G
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define pb push_back
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+
+
+/** 
+ * DATASTRUCTURE: Lazy Segment Tree
+ * PURPOSE: Segment Tree with lazy updates
+ * TIME: O(log n) to update and query
+*/
+template <class Info, class Tag> class LazySegtree {
+  private:
+	const int n;
+	vector<Info> tree;
+	vector<Tag> lazy;
+
+	/** builds the segtree values in O(N) time */
+	void build(int v, int l, int r, const vector<Info> &a) {
+		if (l == r) {
+			tree[v] = a[l];
+		} else {
+			int m = (l + r) / 2;
+			build(2 * v, l, m, a);
+			build(2 * v + 1, m + 1, r, a);
+			tree[v] = tree[2 * v] + tree[2 * v + 1];
+		}
+	}
+
+	/** applies update x to lazy[v] and tree[v] */
+	void apply(int v, int l, int r, const Tag &x) {
+		tree[v].apply(x, l, r);
+		lazy[v].apply(x);
+	}
+
+	/** pushes lazy updates down to the children of v */
+	void push_down(int v, int l, int r) {
+		int m = (l + r) / 2;
+		apply(2 * v, l, m, lazy[v]);
+		apply(2 * v + 1, m + 1, r, lazy[v]);
+		lazy[v] = Tag();
+	}
+
+	void range_update(int v, int l, int r, int ql, int qr, const Tag &x) {
+		if (qr < l || ql > r) { return; }
+		if (ql <= l && r <= qr) {
+			apply(v, l, r, x);
+		} else {
+			push_down(v, l, r);
+			int m = (l + r) / 2;
+			range_update(2 * v, l, m, ql, qr, x);
+			range_update(2 * v + 1, m + 1, r, ql, qr, x);
+			tree[v] = tree[2 * v] + tree[2 * v + 1];
+		}
+	}
+
+	Info range_query(int v, int l, int r, int ql, int qr) {
+		if (qr < l || ql > r) { return Info(); }
+		if (l >= ql && r <= qr) { return tree[v]; }
+		push_down(v, l, r);
+		int m = (l + r) / 2;
+		return range_query(2 * v, l, m, ql, qr) +
+		       range_query(2 * v + 1, m + 1, r, ql, qr);
+	}
+
+  public:
+	LazySegtree() {}
+
+	LazySegtree(int n) : n(n) {
+		tree.assign(4 << __lg(n), Info());
+		lazy.assign(4 << __lg(n), Tag());
+	}
+
+	LazySegtree(const vector<Info> &a) : n(a.size()) {
+		tree.assign(4 << __lg(n), Info());
+		lazy.assign(4 << __lg(n), Tag());
+		build(1, 0, n - 1, a);
+	}
+
+	/** updates [ql, qr] with the arbitrary update chosen */
+	void range_update(int ql, int qr, const Tag &x) {
+		range_update(1, 0, n - 1, ql, qr, x);
+	}
+
+	/** @return result of range query on [ql, qr] */
+	Info range_query(int ql, int qr) { return range_query(1, 0, n - 1, ql, qr); }
+};
+
+enum QueryType { ADD, SET, NONE };
+
+struct Tag {
+	QueryType type = NONE;
+	ll val = 0;
+	void apply(const Tag &t) {
+        if (t.type == ADD) {
+			val += t.val;
+			if (type != SET) { type = ADD; }
+		} else if (t.type == SET) {
+			type = SET;
+			val = t.val;
+		}
+	}
+};
+
+struct Info {
+	pii val = {INT_MIN, INT_MIN};
+	void apply(const Tag &t, int l, int r) {
+		if (t.type == ADD){
+            val.first += t.val;
+            val.second += t.val;
+        } else if (t.type == SET){
+            val = {t.val * (r - l + 1), t.val};
+        }
+	}
+};
+
+/** @return result of joining nodes a and b together */
+Info operator+(const Info &a, const Info &b) {
+    if (a.val.first == INT_MIN){
+        return b;
+    }
+    if (b.val.first == INT_MIN){
+        return a;
+    }
+
+    return Info{{a.val.first + b.val.first, max(a.val.second + b.val.first, b.val.second)}};
+}
+
+template <class T, bool VALS_IN_EDGES> class HLD {
+  public:
+	int N, R, tim = 0;  // n, root node, time
+	vector<vector<int>> adj;
+	vector<int> par, siz, depth, rt, pos, epos;  // parent, size, depth, root, position arrays
+	LazySegtree<Info, Tag> segtree;             // Modify as needed
+
+	/** Compute the size of each subtree and set parent-child relationship
+	 * Subtree of node v corresponds to segment [ pos[v], pos[v] + sz[v] ) */
+	void dfs_sz(int v) {
+		if (par[v] != -1) adj[v].erase(find(adj[v].begin(), adj[v].end(), par[v]));
+		for (int &u : adj[v]) {
+			par[u] = v, depth[u] = depth[v] + 1;
+			dfs_sz(u);
+			siz[v] += siz[u];
+			if (siz[u] > siz[adj[v][0]]) swap(u, adj[v][0]);
+		}
+	}
+
+	/** Assign positions for nodes
+	  * Path from v to the last vertex in ascending heavy path
+	    corresponds to [ pos[rt[v]], pos[v] ] */
+	void dfs_hld(int v) {
+		pos[v] = tim++;
+		for (int u : adj[v]) {
+			rt[u] = (u == adj[v][0] ? rt[v] : u);
+			dfs_hld(u);
+		}
+        epos[v] = tim - 1;
+	}
+
+	/** process all heavy path and combine their results */
+	template <class B> void process(int u, int v, B op) {
+		for (; rt[u] != rt[v]; v = par[rt[v]]) {
+			if (depth[rt[u]] > depth[rt[v]]) swap(u, v);
+			op(pos[rt[v]], pos[v]);
+		}
+		if (depth[u] > depth[v]) swap(u, v);
+		op(pos[u] + VALS_IN_EDGES, pos[v]);
+	}
+
+	HLD(vector<vector<int>> adj_, int _R)
+	    : N(adj_.size()), R(_R), adj(adj_), par(N, -1), siz(N, 1), depth(N), rt(N),
+	      pos(N), epos(N), segtree(N)  // modify as needed
+	{
+		rt[R] = R;
+		dfs_sz(R);
+		dfs_hld(R);
+	}
+
+	int query_path(int u, int v) {
+		int res = INT_MIN;  // default value, modify depending on problem
+        int curr = 0;
+		process(u, v, [&](int l, int r) {
+            pii qr = segtree.range_query(l, r).val;
+            res = max(res, curr + qr.second);
+            curr += qr.first;
+		});
+		return res;
+	}
+
+	void modify_node(int u, T val) { segtree.range_update(pos[u], pos[u], Tag{ADD, val}); }
+    void modify_subtree(int u, T val){
+        segtree.range_update(pos[u], epos[u], Tag{SET, val});
+    }
+};
+
+void solve(){
+    int n, q; cin >> n >> q;
+
+    vector<vi> adj(n);
+
+    rep(i, 0, n - 1){
+        int a; cin >> a;
+        --a;
+        adj[a].pb(i + 1);
+        adj[i + 1].pb(a);
+    }
+
+    HLD<int, false> hld(adj, 0);
+    hld.modify_subtree(0, -1);
+
+    rep(i, 0, q){
+        int c, v; cin >> c >> v;
+        --v;
+        if (c == 1){
+            hld.modify_node(v, 1);
+        } else if (c == 2){
+            hld.modify_subtree(v, -1);
+            int qr = hld.query_path(0, v);
+            if (qr < 0) continue;
+            hld.modify_node(v, -qr - 1);
+        } else {
+            int qr = hld.query_path(0, v);
+            if (qr >= 0){
+                cout << "black" << endl;
+            } else {
+                cout << "white" << endl;
+            }
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
