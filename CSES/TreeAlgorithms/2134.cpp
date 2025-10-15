0d34a1e1f1ab209a132bf845779ff1af518dabe6
polarr <charles.ran9@gmail.com>
2025-10-14 17:09:44 -0700

    practice: cses 2134

---
+++ CSES/TreeAlgorithms/2134.cpp
@@ -0,0 +1,156 @@
+/**
+ * Solution by Charles Ran (polarity.sh)
+ * Date: 2025-10-13
+ * Contest: CSES Problemset
+ * Problem: 2134
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+#define pb push_back
+#define rep(i, a, b) for(int i = (a); i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+
+const int MAX_N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+
+template <class T> class SegmentTree {
+  private:
+	const T UNIT = std::numeric_limits<T>().min();
+
+    T combine(T a, T b){
+        return max(a, b);
+    }
+
+	vector<T> segtree;
+	int len;
+
+  public:
+	SegmentTree(int len) : len(len), segtree(len * 2, UNIT) {}
+
+	void set(int ind, T val) {
+		ind += len;
+		segtree[ind] = val;
+		for (; ind > 1; ind /= 2) {
+			segtree[ind / 2] = combine(segtree[ind], segtree[ind ^ 1]);
+		}
+	}
+
+    // [start, end)
+	T query(int start, int end) {
+		T ans = UNIT;
+		for (start += len, end += len; start < end; start /= 2, end /= 2) {
+			if (start % 2 == 1) { ans = combine(ans, segtree[start++]); }
+			if (end % 2 == 1) { ans = combine(ans, segtree[--end]); }
+		}
+		return ans;
+	}
+};
+
+template <class T, bool VALS_IN_EDGES> class HLD {
+  private:
+	int N, R, tim = 0;  // n, root node, time
+	vector<vector<int>> adj;
+	vector<int> par, siz, depth, rt, pos;  // parent, size, depth, root, position arrays
+	SegmentTree<T> segtree;             // Modify as needed
+
+	/** Compute the size of each subtree and set parent-child relationship
+	 * Subtree of node v corresponds to segment [ pos[v], pos[v] + sz[v] ) */
+	void dfs_sz(int v) {
+		if (par[v] != -1) adj[v].erase(find(adj[v].begin(), adj[v].end(), par[v]));
+		for (int &u : adj[v]) {
+			par[u] = v, depth[u] = depth[v] + 1;
+			dfs_sz(u);
+			siz[v] += siz[u];
+			if (siz[u] > siz[adj[v][0]]) swap(u, adj[v][0]);
+		}
+	}
+
+	/** Assign positions for nodes
+	  * Path from v to the last vertex in ascending heavy path
+	    corresponds to [ pos[rt[v]], pos[v] ] */
+	void dfs_hld(int v) {
+		pos[v] = tim++;
+		for (int u : adj[v]) {
+			rt[u] = (u == adj[v][0] ? rt[v] : u);
+			dfs_hld(u);
+		}
+	}
+
+	/** process all heavy path and combine their results */
+	template <class B> void process(int u, int v, B op) {
+		for (; rt[u] != rt[v]; v = par[rt[v]]) {
+			if (depth[rt[u]] > depth[rt[v]]) swap(u, v);
+			op(pos[rt[v]], pos[v]);
+		}
+		if (depth[u] > depth[v]) swap(u, v);
+		op(pos[u] + VALS_IN_EDGES, pos[v]);
+	}
+
+  public:
+	HLD(vector<vector<int>> adj_, int _R)
+	    : N(adj_.size()), R(_R), adj(adj_), par(N, -1), siz(N, 1), depth(N), rt(N),
+	      pos(N), segtree(N)  // modify as needed
+	{
+		rt[R] = R;
+		dfs_sz(R);
+		dfs_hld(R);
+	}
+
+	T query_path(int u, int v) {
+		T res = 0;  // default value, modify depending on problem
+		process(u, v, [&](int l, int r) {
+			res = max(res, segtree.query(l, r + 1));  // modify depending on problem
+		});
+		return res;
+	}
+
+	void modify_node(int u, T val) { segtree.set(pos[u], val); }
+};
+
+void solve(){
+    int n, q; cin >> n >> q;
+
+    vi vals(n);
+    rep(i, 0, n) cin >> vals[i];
+
+    vector<vi> adj(n);
+
+    rep(i, 0, n - 1){
+        int a, b; cin >> a >> b;
+        --a; --b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+
+    HLD<int, false> hld(adj, 0);
+
+    rep(i, 0, n){
+        hld.modify_node(i, vals[i]);
+    }
+
+    rep(i, 0, q){
+        int c, a, b; cin >> c >> a >> b;
+        --a;
+        if (c == 1){
+            hld.modify_node(a, b);
+        } else {
+            --b;
+            cout << hld.query_path(a, b) << endl;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
